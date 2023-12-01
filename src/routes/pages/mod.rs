124341c011d93c598ecd2b8245a711044dd5406b
Freddy L <neo-ciber_94@hotmail.com>
2023-11-30 21:55:50 -0400

    Added support for dark mode

--- src/routes/pages/mod.rs
+++ src/routes/pages/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use askama::Template;
 use axum::{response::Redirect, routing::get, Router};
 
@@ -9,26 +11,46 @@ pub fn pages_router() -> Router {
         .route("/login", get(login))
 }
 
+enum Theme {
+    Dark,
+    Light,
+}
+
+impl Display for Theme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Theme::Dark => write!(f, "dark"),
+            Theme::Light => write!(f, "light"),
+        }
+    }
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct HomeTemplate {
+    theme: Theme,
     user: User,
 }
 
 async fn home(user: Option<CurrentUser>) -> Result<HomeTemplate, Redirect> {
     match user {
-        Some(CurrentUser(user)) => Ok(HomeTemplate { user }),
+        Some(CurrentUser(user)) => Ok(HomeTemplate {
+            theme: Theme::Dark,
+            user,
+        }),
         _ => Err(Redirect::temporary("/login")),
     }
 }
 
 #[derive(Template)]
 #[template(path = "login.html")]
-struct LoginTemplate;
+struct LoginTemplate {
+    theme: Theme,
+}
 
 async fn login(user: Option<CurrentUser>) -> Result<LoginTemplate, Redirect> {
     match user {
         Some(_) => Err(Redirect::temporary("/")),
-        None => Ok(LoginTemplate),
+        None => Ok(LoginTemplate { theme: Theme::Dark }),
     }
 }
