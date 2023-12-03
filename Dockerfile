061fb149569e22522b2b61afed6fb87641bb81ee
Freddy L <neo-ciber_94@hotmail.com>
2023-12-03 19:13:29 -0400

    Added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+# Use the official Rust 1.74 image as the base image
+FROM rust:1.74 as builder
+
+# Set the working directory in the container
+WORKDIR /app/src
+
+# Install SQLx CLI
+RUN cargo install sqlx-cli@0.7.3
+
+# Install nvm and Node.js 18
+RUN apt-get update && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash 
+
+ENV NVM_DIR=/root/.nvm
+ENV NODE_VERSION=18.17.0
+RUN . "$NVM_DIR/nvm.sh" && \
+    nvm install ${NODE_VERSION} && \
+    nvm use ${NODE_VERSION} && \
+    nvm alias default ${NODE_VERSION}
+
+ENV PATH="/root/.nvm/versions/node/v${NODE_VERSION}/bin/:${PATH}"
+
+# Check Node.js
+RUN node --version
+
+# Copy the necessary files for SQLx migrations
+COPY ./migrations ./migrations
+
+# Create and apply the migration
+RUN mkdir data && sqlx database create --database-url "sqlite:./data/data.db"
+RUN sqlx migrate run --database-url "sqlite:./data/data.db"
+
+# Copy the entire project to the container
+COPY . .
+
+# Run the tw:build script
+RUN npm install && npm run tw:build
+
+# Build the Rust project
+RUN export DATABASE_URL="sqlite:./data/data.db" && cargo build --release
+
+# Create a new image
+FROM ubuntu:24.04 as runner
+RUN apt-get update && apt-get install openssl -y
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy necessary files to run the binary
+COPY --from=builder /app/src/target/release/axum-oauth-sample /app/axum-oauth-sample
+COPY --from=builder /app/src/data /app/data
+COPY --from=builder /app/src/public /app/public
+COPY --from=builder /app/src/templates /app/templates
+
+# Run the Rust project
+CMD ["./axum-oauth-sample"]
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod models;
 mod routes;
 mod server;
 
+use anyhow::Context;
 use axum::{Extension, Router};
 use dotenvy::dotenv;
 use sqlx::sqlite::SqlitePool;
@@ -20,8 +21,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_max_level(Level::DEBUG)
         .init();
 
-    let connection_string = std::env::var("DATABASE_URL")?;
-    let pool = SqlitePool::connect(&connection_string).await?;
+    let connection_string = std::env::var("DATABASE_URL").context("'DATABASE_URL' no found")?;
+    let pool = SqlitePool::connect(&connection_string)
+        .await
+        .context("Failed to connect to database")?;
 
     let app = Router::new()
         .merge(crate::routes::api_router())
@@ -31,21 +34,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .layer(TraceLayer::new_for_http());
 
     let (host, port) = get_host_and_port()?;
-    let listener = tokio::net::TcpListener::bind((host.as_str(), port)).await?;
-    tokio::net::TcpListener::bind((host.as_str(), port)).await?;
+    let listener = tokio::net::TcpListener::bind((host.as_str(), port))
+        .await
+        .context("Failed to start tcp listener")?;
+
     println!("Listening on: http://{host}:{port}");
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app)
+        .await
+        .context("Failed to start server")?;
 
     Ok(())
 }
 
 fn get_host_and_port() -> Result<(String, u16), Box<dyn Error>> {
-    let host = std::env::var("HOST").map_err(|_| "PORT not found".to_owned())?;
+    let host = std::env::var("HOST").context("'HOST' no found")?;
     let port = std::env::var("PORT")
-        .map_err(|_| "PORT not found".to_owned())
+        .context("'PORT' no found")
         .and_then(|x| match x.parse::<u16>() {
             Ok(port) => Ok(port),
-            Err(_) => Err(format!("Invalid port: {x}")),
+            Err(_) => Err(anyhow::anyhow!("Invalid port: {x}")),
         })?;
 
     Ok((host, port))
